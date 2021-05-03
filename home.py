c371f7baeab5b5e844173bcb431f2da0461093f5
Lakshya Agarwal <lakshayagarwal3214@gmail.com>
2021-05-03 11:26:11 +0530

    update homepage

--- home.py
+++ home.py
@@ -1,25 +1,7 @@
 import urllib
-import xlrd
 import pandas as pd
 import numpy as np
 import streamlit as st
-import yfinance as yf
-import seaborn as sns
-import plotly.graph_objs as go
-from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-import matplotlib.pyplot as plt
-plt.style.use('bmh')
-import sqlite3
-conn=sqlite3.connect('Data.db')
-c=conn.cursor()
-import quandl
-import matplotlib.animation as ani
-import altair as alt
 
 ############################################################################
 def main():
